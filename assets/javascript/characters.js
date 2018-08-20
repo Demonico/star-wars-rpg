700cf96d362fc1ba70f115636fd7e83a04313d53
Jarrod <Demonico@users.noreply.github.com>
2018-08-20 16:54:33 -0600

    added character.js and images

---
+++ assets/javascript/characters.js
@@ -0,0 +1,26 @@
+module.exports = {
+ luke: {
+   hp_base: 50,
+   attack_base: 6,
+   counter_attack: 10,
+   img: 'Luke-Skywalker.png'
+ },
+ maul: {
+  hp_base: 125,
+  attack_base: 12,
+  counter_attack: 20,
+  img: 'Darth-Maul.png'
+ },
+ sidius: {
+  hp_base: 100,
+  attack_base: 20,
+  counter_attack: 25,
+  img: 'Darth-Sidious.jpg'
+ },
+ obi_wan: {
+  hp_base: 75,
+  attack_base: 10,
+  counter_attack: 15,
+  img: 'Obi-Wan-Kenobi.png'
+ }
+}
---
+++ assets/javascript/game.js
@@ -0,0 +1,2 @@
+const characters = require('./characters');
+
